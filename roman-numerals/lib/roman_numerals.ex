9ab722c79a70bb221f54b222c4e42a6f6755f1d0
Luiz Gustavo Caciatori <luizgustavo.caciatori@gmail.com>
2020-05-11 18:37:09 -0300

    Refactored the numeral function

--- roman-numerals/lib/roman_numerals.ex
+++ roman-numerals/lib/roman_numerals.ex
@@ -1,109 +1,42 @@
 defmodule RomanNumerals do
-  @romans %{
-    1 => "I",
-    4 => "IV",
-    5 => "V",
-    9 => "IX",
-    10 => "X",
-    40 => "XL",
-    50 => "L",
-    90 => "XC",
-    100 => "C",
-    400 => "CD",
-    500 => "D",
-    900 => "CM",
-    1000 => "M"
-  }
+  @romans [
+    {1000, "M"},
+    {900, "CM"},
+    {500, "D"},
+    {400, "CD"},
+    {100, "C"},
+    {90, "XC"},
+    {50, "L"},
+    {40, "XL"},
+    {10, "X"},
+    {9, "IX"},
+    {5, "V"},
+    {4, "IV"},
+    {1, "I"},
+  ]
 
   @doc """
   Convert the number to a roman number.
   """
   @spec numeral(pos_integer) :: String.t()
   def numeral(number) do
-    convert_to_roman(number, "")
+    transform(number, @romans)
   end
 
-  def convert_to_roman(0, acc) do
-    acc
-  end
+  defp transform(value, roman_symbols, acc \\ "")
+  defp transform(_value, [], acc), do: acc
+  defp transform(value, roman_symbols, acc) do
+    [current | rest] = roman_symbols
 
-  def convert_to_roman(number, acc) when div(number, 1000) > 0 do
-    acc = acc <> @romans[1000]
+    {number, symbol} = current
 
-    convert_to_roman(number - 1000, acc)
-  end
+    result = div(value, number)
 
-  def convert_to_roman(number, acc) when div(number, 900) > 0 do
-    acc = acc <> duplicate(@romans[900], div(number, 900))
-
-    convert_to_roman(number - 900, acc)
-  end
-
-  def convert_to_roman(number, acc) when div(number, 500) > 0 do
-    acc = acc <> duplicate(@romans[500], div(number, 500))
-
-    convert_to_roman(number - 500, acc)
-  end
-
-  def convert_to_roman(number, acc) when div(number, 400) > 0 do
-    acc = acc <> duplicate(@romans[400], div(number, 400))
-
-    convert_to_roman(number - 400, acc)
-  end
-
-  def convert_to_roman(number, acc) when div(number, 100) > 0 do
-    acc = acc <> duplicate(@romans[100], div(number, 100))
-
-    convert_to_roman(number - 100, acc)
-  end
-
-  def convert_to_roman(number, acc) when div(number, 90) > 0 do
-    acc = acc <> duplicate(@romans[90], div(number, 90))
-
-    convert_to_roman(number - 90, acc)
-  end
-
-  def convert_to_roman(number, acc) when div(number, 50) > 0 do
-    acc = acc <> duplicate(@romans[50], div(number, 50))
-
-    convert_to_roman(number - 50, acc)
-  end
-
-  def convert_to_roman(number, acc) when div(number, 40) > 0 do
-    acc = acc <> duplicate(@romans[40], div(number, 40))
-
-    convert_to_roman(number - 40, acc)
-  end
-
-  def convert_to_roman(number, acc) when div(number, 10) > 0 do
-    result = div(number, 10)
-
-    acc = acc <> duplicate(@romans[10], result)
-
-    convert_to_roman(number - (10 * result) , acc)
-  end
-
-  def convert_to_roman(number, acc) when div(number, 9) > 0 do
-    acc = acc <> duplicate(@romans[9], div(number, 9))
-
-    convert_to_roman(number - 9, acc)
-  end
-
-  def convert_to_roman(number, acc) when div(number, 5) > 0 do
-    acc = acc <> duplicate(@romans[5], div(number, 5))
-
-    convert_to_roman(number - 5, acc)
-  end
-
-  def convert_to_roman(number, acc) when div(number, 4) > 0 do
-    acc <> duplicate(@romans[4], div(number, 4))
-  end
-
-  def convert_to_roman(number, acc) when div(number, 1) > 0 do
-    acc <> duplicate(@romans[1], div(number, 1))
-  end
-
-  def duplicate(char, times) do
-    String.duplicate(char, times)
+    if result > 0 do
+      acc = acc <> String.duplicate(symbol, result)
+      transform(value - (number * result), rest, acc)
+    else
+      transform(value, rest, acc)
+    end
   end
 end
