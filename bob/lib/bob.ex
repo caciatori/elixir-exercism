867e2bee928447c17ccd92942b5093072214cd3a
Luiz Gustavo Caciatori <luizgustavo.caciatori@gmail.com>
2020-05-23 11:05:50 -0300

    Added conditional to split downcase string for upcase string in all_upcase

--- bob/lib/bob.ex
+++ bob/lib/bob.ex
@@ -7,12 +7,34 @@ defmodule Bob do
     whatever = "Whatever."
 
     input = String.trim(input)
+
     cond do
-      input =~ ~r/(^[A-Z]+\W)/ and input =~ ~r/\?$/ -> calm_down
-      input == "" or input =~ ~r/(^[[:space:]])/ -> fine
-      input =~ ~r/\?$/ -> sure
-      input =~ ~r/([[:upper:]]+$)/u or input =~ ~r/(([A-Z0-9]+!$))/ -> chill_out
-      true -> whatever
+      calm_down?(input) ->
+        calm_down
+
+      empty?(input) ->
+        fine
+
+      question?(input) ->
+        sure
+
+      chill_out?(input) ->
+        chill_out
+
+      true ->
+        whatever
     end
+  end
+
+  defp empty?(input), do: "" == input
+
+  defp question?(input), do: String.ends_with?(input, "?")
+
+  defp all_upcase?(input), do: String.upcase(input) == input and String.downcase(input) != input
+
+  defp calm_down?(input), do: all_upcase?(input) and String.ends_with?(input, "?")
+
+  defp chill_out?(input) do
+    (all_upcase?(input) and String.ends_with?(input, "!")) or all_upcase?(input)
   end
 end
